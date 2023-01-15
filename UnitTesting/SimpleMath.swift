74c22c05f264a69195ee84281112b6cf895a56a2
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-01-15 12:07:55 +0530

    Unit testing learned.

---
+++ UnitTesting/SimpleMath.swift
@@ -0,0 +1,19 @@
+//
+//  SimpleMath.swift
+//  UnitTesting
+//
+//  Created by Sajed Shaikh on 15/01/23.
+//
+
+import Foundation
+
+class SimpleMath {
+    
+    func addTen(number: Int) -> Int {
+        return number + 10
+    }
+    
+    func beSquare(_ number: Int) -> Int {
+        return number * number
+    }
+}
--- UnitTestingTests/UnitTestingTests.swift
+++ UnitTestingTests/UnitTestingTests.swift
@@ -9,28 +9,41 @@ import XCTest
 @testable import UnitTesting
 
 final class UnitTestingTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    var simpleMath: SimpleMath!
+    
+    override func setUp() {
+        simpleMath = SimpleMath()
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    override func tearDown() {
+        simpleMath = nil
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testAddTenWorks() {
+        // Given - Arrange
+        var extected: Int = 0
+        
+        // When - Act
+        extected = simpleMath.addTen(number: 10)
+        
+        // Then - Assert
+        XCTAssert(extected == 20, "This should be 20")
+        XCTAssertTrue(extected == 20)
+        XCTAssertEqual(extected, 20, "This should be 20")
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func testBeSquareSucceeds() {
+        // Given - Arrange
+        var expected: Int = 0
+        
+        // When - Act
+        expected = simpleMath.beSquare(4)
+        
+        // Then - Assert
+        XCTAssert(expected == 16, "This should be 16")
+        XCTAssertTrue(expected == 16)
+        XCTAssertEqual(expected, 16, "This should be 16")
     }
 
 }
