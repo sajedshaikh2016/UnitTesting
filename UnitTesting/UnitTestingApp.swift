8ed43f52f8670cba81563a1d1c40cf315a24d73e
Sajed Salim Shaikh <sajed.shaikh@hotmail.com>
2023-01-15 11:22:17 +0530

    Initial Commit

---
+++ UnitTesting/UnitTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestingApp.swift
+//  UnitTesting
+//
+//  Created by Sajed Shaikh on 15/01/23.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
